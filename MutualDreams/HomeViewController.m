533b1cb824d61dd4e0002210824ae8d2147dc472
ncadena <ncadena@iMac-de-idadministrator-2.local>
2014-07-22 11:11:39 -0500

    Nuevos Controllers

--- MutualDreams/HomeViewController.m
+++ MutualDreams/HomeViewController.m
@@ -2,7 +2,7 @@
 //  HomeViewController.m
 //  MutualDreams
 //
-//  Created by Nicolas Cadena on 18/07/14.
+//  Created by Nicolas Cadena on 22/07/14.
 //  Copyright (c) 2014 Nicolas Cadena. All rights reserved.
 //
 
